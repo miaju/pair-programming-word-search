ccf7e02f62724860b61283661337ee53b496a0b3
Mia Tanner <mia@indygecko.com>
2022-09-07 10:38:15 -0700

    finish implementation of word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = matrix => {
+  let result = [];
+  const width = matrix[0].length;
+  
+  for (let i = 0; i < width; i++) {
+    const newRow = [];
+    for (const item of matrix) {
+      newRow.push(item[i]);
     }
-}
+    result.push(newRow);
+  }
+  return result;
+};
 
-module.exports = wordSearch
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
